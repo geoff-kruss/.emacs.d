df1a07f048b7278b3b5fd1b365a1325bbbc303f5
geoff kruss <geoff.kruss@yuppiechef.com>
2021-06-23 17:07:59 +0200

    changes

--- init.el
+++ init.el
@@ -20,11 +20,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (flycheck-joker doom-themes smartparens-config clojure-mode smartparens helpful dired-open markdown-mode yaml-mode company-terraform terraform-mode elpy python-mode flycheck-package rainbow-delimiters paredit coffee-mode scss-mode undo-tree projectile company avy ag minions moody auto-compile use-package))))
+   '(kubernetes neotree flycheck-joker doom-themes smartparens-config clojure-mode smartparens helpful dired-open markdown-mode yaml-mode company-terraform terraform-mode elpy python-mode flycheck-package rainbow-delimiters paredit coffee-mode scss-mode undo-tree projectile company avy ag minions moody auto-compile use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(markdown-code-face ((t nil))))
